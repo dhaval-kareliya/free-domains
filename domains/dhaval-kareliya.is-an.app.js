e91439b9f61f8089594710505b0b1f8c395f18b3
dhaval-kareliya <dhaval.kareliya@truestaz.com>
2023-04-21 16:45:47 +0530

    Add .is-an.app.js File

---
+++ domains/dhaval-kareliya.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'I want to publish my profile',
+  domain: 'is-an.app',
+  subdomain: 'dhaval-kareliya',
+  owner: {
+    repo: 'https://github.com/dhaval-kareliya/dhaval-kareliya.github.io',
+    email: 'dhaval.kareliya@truestaz.com',
+  },
+  record: {
+    CNAME: 'dhaval-kareliya.github.io',
+  },
+  proxy: false,
+})
